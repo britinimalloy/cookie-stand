f64c2d74bf1bd4da6f951d9a5edc38af9002169b
britinimalloy <britini.malloy@outlook.com>
2017-06-13 22:46:27 -0700

    cleaned up the code from app.js to remove non-essential code notes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var hours = [' 6 am ',' 7 am ',' 8 am ',' 9 am ',' 10 am ',' 11 am ' ,' 12 pm ',' 1 pm ',' 2 pm ',' 3 pm ',' 4 pm ',' 5 pm ',' 6 pm ',' 7 pm ',' 8 pm ', ' Total '];
 console.log(hours);
 
+// get random numbers
 var findRandomCookies = function (store) {
   var total = 0;
   var cookieSales = [];
@@ -13,11 +14,11 @@ var findRandomCookies = function (store) {
   }
   cookieSales.push(total);
   console.log(total);
-  //store.randomCookies = cookieSales;
   console.log(cookieSales);
   return cookieSales;
 };
-// ===============================================================
+
+// object "factory" LOL
 function CreateStores (name, minCust, maxCust, avgCookies) {
   this.name = name;
   this.minCust = minCust;
@@ -26,34 +27,24 @@ function CreateStores (name, minCust, maxCust, avgCookies) {
   this.randomCookies = findRandomCookies(this);
 }
 
+// function to create and populate the rows of the table
 CreateStores.prototype.render = function(table) {
-  // create a row eith a header for the store name,
-  // enough columns (td) to be hours.length
-  // to hold the generated numbers for each hours
-  // needs the last column to a total
-  // append this row to the table
   var random = findRandomCookies(this);
-  var row = document.createElement('tr'); //this is the parent for th and td elements
+  var row = document.createElement('tr');
   table.appendChild(row);
 
   var th = document.createElement('th');
-  //this is for the name of the store
   th.textContent = this.name;
   row.appendChild(th);
 
-  //make a for loop to create enough td for the whole array
   for (var i = 0; i < 16; i++) {
     var td1 = document.createElement('td');
-    //var li = document.createElement('li');
-    //li.textContent = hours[i] + random[i];
-    td1.textContent = /*hours[i] +*/ random[i];
-    //ul.appendChild(li);
+    td1.textContent = random[i];
     row.appendChild(td1);
   }
-  //var td1 = document.createElement('td');
-  //row.appendChild(td1);
 };
-// ===============================================================
+
+// instantiate all the objects
 var FirstAndPike = new CreateStores ('First And Pike', 23, 65, 6.3);
 console.log(FirstAndPike);
 
@@ -69,38 +60,10 @@ console.log(CapitolHill);
 var Alki = new CreateStores ('Alki', 2, 16, 4.6);
 console.log(Alki);
 
-// ==============================================
-/*var getStoreInfo = function (store) {
-  var random = findRandomCookies(store);
-
-  var parentElement = document.getElementById('locations');
-
-  var article = document.createElement('article');
-  parentElement.appendChild(article);
-
-  var h2 = document.createElement('h2');
-  h2.textContent = store.name;
-  article.appendChild(h2);
-
-  var ul = document.createElement('ul');
-  article.appendChild(ul);
-
-  for (var i = 0; i < 16; i++) {
-    var li = document.createElement('li');
-    li.textContent = hours[i] + random[i];
-    ul.appendChild(li);
-  }
-};*/
-// ==============================================
-//getStoreInfo(FirstAndPike);
 tableRender ();
-//getStoreInfo(SeaTacAirport);
-//getStoreInfo(SeattleCenter);
-//(CapitolHill);
-//getStoreInfo(Alki);
 
-// call the function to create the table
-function tableRender () { //to create the table
+// function to create the table
+function tableRender () {
   var parentElement = document.getElementById('locations');
   var table = document.createElement('table');
   parentElement.appendChild(table);
@@ -120,5 +83,4 @@ function tableRender () { //to create the table
   SeattleCenter.render(table);
   CapitolHill.render(table);
   Alki.render(table);
-  //
 }
