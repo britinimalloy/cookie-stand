2d93faf9dabef76623c5730e4a011253bc1f2c89
britinimalloy <britini.malloy@outlook.com>
2017-06-13 18:05:03 -0700

    all MVP's added and workinggit status

--- js/app.js
+++ js/app.js
@@ -25,6 +25,34 @@ function CreateStores (name, minCust, maxCust, avgCookies) {
   this.avgCookies = avgCookies;
   this.randomCookies = findRandomCookies(this);
 }
+
+CreateStores.prototype.render = function(table) {
+  // create a row eith a header for the store name,
+  // enough columns (td) to be hours.length
+  // to hold the generated numbers for each hours
+  // needs the last column to a total
+  // append this row to the table
+  var random = findRandomCookies(this);
+  var row = document.createElement('tr'); //this is the parent for th and td elements
+  table.appendChild(row);
+
+  var th = document.createElement('th');
+  //this is for the name of the store
+  th.textContent = this.name;
+  row.appendChild(th);
+
+  //make a for loop to create enough td for the whole array
+  for (var i = 0; i < 16; i++) {
+    var td1 = document.createElement('td');
+    //var li = document.createElement('li');
+    //li.textContent = hours[i] + random[i];
+    td1.textContent = /*hours[i] +*/ random[i];
+    //ul.appendChild(li);
+    row.appendChild(td1);
+  }
+  //var td1 = document.createElement('td');
+  //row.appendChild(td1);
+};
 // ===============================================================
 var FirstAndPike = new CreateStores ('First And Pike', 23, 65, 6.3);
 console.log(FirstAndPike);
@@ -41,7 +69,8 @@ console.log(CapitolHill);
 var Alki = new CreateStores ('Alki', 2, 16, 4.6);
 console.log(Alki);
 
-var getStoreInfo = function (store) {
+// ==============================================
+/*var getStoreInfo = function (store) {
   var random = findRandomCookies(store);
 
   var parentElement = document.getElementById('locations');
@@ -61,10 +90,35 @@ var getStoreInfo = function (store) {
     li.textContent = hours[i] + random[i];
     ul.appendChild(li);
   }
-};
+};*/
+// ==============================================
+//getStoreInfo(FirstAndPike);
+tableRender ();
+//getStoreInfo(SeaTacAirport);
+//getStoreInfo(SeattleCenter);
+//(CapitolHill);
+//getStoreInfo(Alki);
 
-getStoreInfo(FirstAndPike);
-getStoreInfo(SeaTacAirport);
-getStoreInfo(SeattleCenter);
-getStoreInfo(CapitolHill);
-getStoreInfo(Alki);
+// call the function to create the table
+function tableRender () { //to create the table
+  var parentElement = document.getElementById('locations');
+  var table = document.createElement('table');
+  parentElement.appendChild(table);
+  var row1 = document.createElement('tr');
+  table.appendChild(row1);
+  var th = document.createElement('th');
+  row1.appendChild(th);
+
+  for (var i = 0; i < 16; i++) {
+    var th1 = document.createElement('th');
+    th1.textContent = hours[i];
+    row1.appendChild(th1);
+  }
+
+  FirstAndPike.render(table);
+  SeaTacAirport.render(table);
+  SeattleCenter.render(table);
+  CapitolHill.render(table);
+  Alki.render(table);
+  //
+}
